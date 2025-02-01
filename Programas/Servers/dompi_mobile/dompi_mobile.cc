3ecf4651fd4662c6927445c6cfd7398afbbe39f2
Walter Pirri <walter@pirri.com.ar>
2025-02-01 19:02:47 -0300

    Nuevo modulo para mensajes push
    Nuevo modulo para mensajes push

---
+++ Programas/Servers/dompi_mobile/dompi_mobile.cc
@@ -0,0 +1,157 @@
+/***************************************************************************
+    Copyright (C) 2021   Walter Pirri
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ***************************************************************************/
+#include <gmonitor/gmerror.h>
+#include <gmonitor/gmontdb.h>
+/*#include <gmonitor/gmstring.h>*/
+#include <gmonitor/gmswaited.h>
+
+#include <string>
+#include <iostream>
+#include <csignal>
+using namespace std;
+
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cjson/cJSON.h>
+
+#include "config.h"
+#include "strfunc.h"
+//#include "cpgdb.h"
+#include "cmydb.h"
+
+#define MAX_BUFFER_LEN 32767
+
+CGMServerWait *m_pServer;
+DPConfig *pConfig;
+//CPgDB *pDB;
+CMyDB *pDB;
+
+void OnClose(int sig);
+
+int main(/*int argc, char** argv, char** env*/void)
+{
+	int rc;
+	char fn[33];
+	char typ[1];
+	char message[MAX_BUFFER_LEN+1];
+	unsigned long message_len;
+
+	char db_host[32];
+	char db_name[32];
+	char db_user[32];
+	char db_password[32];
+	char query[4096];
+
+	time_t t;
+
+	STRFunc Strf;
+
+    cJSON *json_Message;
+
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGKILL, OnClose);
+	signal(SIGTERM, OnClose);
+
+	m_pServer = new CGMServerWait;
+	m_pServer->Init("dompi_mobile");
+	m_pServer->m_pLog->Add(1, "Iniciando Servicios Mobiles...");
+
+	m_pServer->m_pLog->Add(10, "Leyendo configuración...");
+	pConfig = new DPConfig("/etc/dompicloud.config");
+	pConfig->GetParam("DBHOST", db_host);
+	pConfig->GetParam("DBNAME", db_name);
+	pConfig->GetParam("DBUSER", db_user);
+	pConfig->GetParam("DBPASSWORD", db_password);
+
+	m_pServer->Suscribe("dompi_cloud_difusion", GM_MSG_TYPE_CR);
+
+	m_pServer->m_pLog->Add(10, "Conectado a la base de datos...");
+	pDB = new CMyDB(db_host, db_name, db_user, db_password);
+	if(pDB == NULL)
+	{
+		m_pServer->m_pLog->Add(1, "ERROR: Al conectarse a la base de datos.");
+	}
+	if(pDB->Open() != 0)
+	{
+		m_pServer->m_pLog->Add(1, "ERROR: En Open a la base de datos.");
+	}
+
+	m_pServer->m_pLog->Add(1, "Servicios de Domotica en la nube inicializados.");
+
+	while((rc = m_pServer->Wait(fn, typ, message, MAX_BUFFER_LEN, &message_len, (-1) )) >= 0)
+	{
+		if(rc > 0)
+		{
+			message[message_len] = 0;
+			m_pServer->m_pLog->Add(100, "%s:(Q)[%s]", fn, message);
+			/* ****************************************************************
+			*		dompi_web_notif - 
+			**************************************************************** */
+			if( !strcmp(fn, "dompi_cloud_difusion"))
+			{
+				t = time(&t);
+
+				json_Message = cJSON_Parse(message);
+				message[0] = 0;
+
+
+
+				/* Siempre responder OK para evitar skimming */
+				strcpy(message, "{\"response\":{\"resp_code\":\"0\", \"resp_msg\":\"Ok\"}}");
+
+
+
+
+				m_pServer->m_pLog->Add(50, "%s:(R)[%s]", fn, message);
+				if(m_pServer->Resp(message, strlen(message), GME_OK) != GME_OK)
+				{
+					/* error al responder */
+					m_pServer->m_pLog->Add(1, "ERROR al responder mensaje [%s]", fn);
+				}
+				cJSON_Delete(json_Message);
+			}
+
+
+
+			else
+			{
+				m_pServer->m_pLog->Add(50, "GME_SVC_NOTFOUND");
+				m_pServer->Resp(NULL, 0, GME_SVC_NOTFOUND);
+			}
+		}
+
+
+	}
+	m_pServer->m_pLog->Add(1, "ERROR en la espera de mensajes");
+	OnClose(0);
+	return 0;
+}
+
+void OnClose(int sig)
+{
+	m_pServer->m_pLog->Add(1, "Exit on signal %i", sig);
+	m_pServer->UnSuscribe("dompi_cloud_difusion", GM_MSG_TYPE_CR);
+
+	delete m_pServer;
+	delete pConfig;
+	delete pDB;
+	exit(0);
+}
